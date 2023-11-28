19816ed2bdb234b3ea4464b0c1794af976124e38
Mafiv <150416709+Mafiv@users.noreply.github.com>
2023-11-28 12:39:13 +0200

    Create sum_of_square.py

---
+++ sum_of_square.py
@@ -0,0 +1,19 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        p=0
+        z=int(c**0.5)
+        while(True):
+            val=p**2+z**2
+            if(val==c):
+                return True
+                break
+            elif(p==z):
+                if(val==c):
+                    return True
+                else:
+                    return False
+                break
+            elif(val<c):
+                p+=1
+            elif(val>c):
+                z-=1
