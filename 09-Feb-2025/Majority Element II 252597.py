f2c3c732b1453d2288cd92ece65de78631ed4d7e
Mafiv <150416709+Mafiv@users.noreply.github.com>
2025-02-10 01:08:52 +0300

    Created 'Majority Element II' by A2SV Hub

---
+++ 09-Feb-2025/Majority Element II 252597.py
@@ -0,0 +1,11 @@
+# Problem: Majority Element II - https://leetcode.com/problems/majority-element-ii/?envType=daily-question&envId=2023-10-05
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        count=Counter(nums)
+        n=len(nums)/3
+        ans=[]
+        for i in count:
+            if(count[i]>n):
+                ans.append(i)
+        return ans
