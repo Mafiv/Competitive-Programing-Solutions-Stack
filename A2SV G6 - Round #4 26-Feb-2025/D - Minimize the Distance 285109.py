7286ba412cbf767bbd5085547cc1fdcc687f30e8
Mafiv <150416709+Mafiv@users.noreply.github.com>
2025-02-26 19:13:11 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/D - Minimize the Distance 285109.py
@@ -0,0 +1,12 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+def solve():
+    x=list(map(int,input().split()))
+    x.sort()
+    a=len(x)
+    if(a%2==0):
+        return x[a//2-1]
+    return x[a//2]
+if __name__=="__main__":
+    x=int(input())
+    print(solve())
