a9e95fe952ba2d46172c6cb1e13aa0ede8cbaf50
Mafiv <150416709+Mafiv@users.noreply.github.com>
2025-03-14 22:35:43 +0300

    Created 'B - Nafargi's Binary Reduction Game' by A2SV Hub

---
+++ A2SV G6 - Round #6 14-Mar-2025/B - Nafargi's Binary Reduction Game 307446.py
@@ -0,0 +1,15 @@
+# Problem: B - Nafargi's Binary Reduction Game - https://codeforces.com/gym/594077/problem/B
+
+from collections import Counter
+def solve():
+  a=int(input())
+  x=input()
+  c=Counter(str(x))
+  res=abs(c['0']-c['1'])
+  return res
+
+
+if __name__=="__main__":
+    x=1
+    for i in range(x):
+        print(solve())
