50a189dad2aaffd55a75bf87f22a67c722ea4dd0
Mafiv <150416709+Mafiv@users.noreply.github.com>
2025-03-28 17:13:35 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 28-Mar-2025/F - Binary Substrings with Exactly k Ones 320863.py
@@ -0,0 +1,25 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+def sub_for_k(k,arr):
+    if(k==-1):
+        return 0
+    l=0
+    count=0
+    ans=0
+    for i in range(len(arr)):
+        if(arr[i]=='1'):
+            count+=1
+        while(count>k):
+            if(arr[l]=='1'):
+                count-=1
+            l+=1
+        ans+=i-l+1
+    return ans
+
+def solve():
+    x=int(input())
+    y=input() 
+    return (sub_for_k(x,y)-sub_for_k(x-1,y))
+
+if __name__=="__main__":
+    print(solve())
