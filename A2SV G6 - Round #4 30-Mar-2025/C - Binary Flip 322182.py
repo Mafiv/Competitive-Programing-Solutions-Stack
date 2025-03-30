3d468b0da0d7845db372aa9cfee6a9e3e8a89a9b
Mafiv <150416709+Mafiv@users.noreply.github.com>
2025-03-30 22:36:39 +0300

    Created 'C - Binary Flip' by A2SV Hub

---
+++ A2SV G6 - Round #4 30-Mar-2025/C - Binary Flip 322182.py
@@ -0,0 +1,27 @@
+# Problem: C - Binary Flip - https://codeforces.com/gym/590053/problem/C
+
+
+def solve():
+    n = int(input())
+    s1 = input()
+    s2 = input()
+    s1 += '0'
+    s2 += '0'
+    count = 0    
+    for i in range(n):
+        if s1[i] == '1':
+            count += 1
+        else:
+            count -= 1        
+        if count != 0:
+            if s1[i] != s2[i] and s1[i+1] == s2[i+1]:
+                print("NO")
+                return            
+            elif s1[i] == s2[i] and s1[i+1] != s2[i+1]:
+                print("NO")
+                return    
+    print("YES")
+
+t = int(input())
+for _ in range(t):
+    solve()
