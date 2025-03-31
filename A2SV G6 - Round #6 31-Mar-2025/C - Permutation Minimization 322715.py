a1870d772ff6a13b6c55dfeef6b154893c0de52a
Mafiv <150416709+Mafiv@users.noreply.github.com>
2025-03-31 20:07:22 +0300

    Created 'C - Permutation Minimization' by A2SV Hub

---
+++ A2SV G6 - Round #6 31-Mar-2025/C - Permutation Minimization 322715.py
@@ -0,0 +1,19 @@
+# Problem: C - Permutation Minimization - https://codeforces.com/gym/594077/problem/C
+
+from collections import deque
+def solve():
+  a=int(input())
+  x=list(map(int,input().split()))
+  ans=deque()
+  ans.append(x[0])
+  for i in range(1,a):
+    if(x[i]>ans[0]):
+        ans.append(x[i])
+    else:
+        ans.appendleft(x[i])
+  return ans
+
+if __name__=="__main__":
+    x=int(input())
+    for i in range(x):
+        print(*(solve()))
