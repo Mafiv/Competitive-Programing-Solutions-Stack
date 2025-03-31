a495b2a9c425ae394445aa2bbf2e6f0fd10368e4
Mafiv <150416709+Mafiv@users.noreply.github.com>
2025-03-31 19:50:11 +0300

    Created 'D - Life Across the Stars' by A2SV Hub

---
+++ A2SV G6 - Round #5 31-Mar-2025/D - Life Across the Stars 322699.py
@@ -0,0 +1,24 @@
+# Problem: D - Life Across the Stars - https://codeforces.com/gym/591928/problem/D
+
+from collections import defaultdict
+def solve():
+    i=int(input())
+    dicts=defaultdict(int)
+    for j in range(i):
+        x=list(map(int,input().split()))
+        dicts[x[0]]+=1
+        dicts[x[1]]-=1
+    p=sorted(dicts.keys())
+    population=0
+    max_p=0
+    year=p[0]
+    for i in p:
+        population+=dicts[i]
+        if(population > max_p):
+            year=i
+            max_p=population
+    return year,max_p
+if __name__=="__main__":
+    i=1
+    for _ in range(i):
+        print(*(solve()))
