4c6e2c048ae85c7a73651afc9bf0aef429da7829
Mafiv <150416709+Mafiv@users.noreply.github.com>
2025-03-31 20:13:01 +0300

    Created 'D - Meklit's Chat Screen' by A2SV Hub

---
+++ A2SV G6 - Round #6 31-Mar-2025/D - Meklit's Chat Screen 322719.py
@@ -0,0 +1,23 @@
+# Problem: D - Meklit's Chat Screen - https://codeforces.com/gym/594077/problem/D
+
+from collections import deque
+def solve():
+    n, k = map(int, input().split())
+    messages = list(map(int, input().split()))
+    screen = deque()
+    seen = set()
+
+    for id_i in messages:
+        if id_i not in seen:
+            if len(screen) == k:
+                removed = screen.pop()
+                seen.remove(removed)
+            
+            screen.appendleft(id_i)
+            seen.add(id_i)
+
+    print(len(screen))
+    return screen
+
+if __name__=="__main__":
+    print(*(solve()))
